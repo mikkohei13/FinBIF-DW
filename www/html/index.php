d96c4cc735eae5ae0ea5da595951731e069d3dba
Mikko Heikkinen <hawk@biomi.org>
2019-09-09 21:41:10 +0300

    Move mockup API and other tools from piipitin

--- www/html/index.php
+++ www/html/index.php
@@ -1,6 +1,14 @@
 <?php
 
 require_once "mysql.php";
+require_once "readINat.php";
+require_once "postToAPI.php";
+require_once "log2.php";
+
+log2("ok", "Script started");
+
+
+// Database
 
 $database = new mysqlDb();
 $database->connect("inat_push");
@@ -13,8 +21,29 @@ $database->set_latest_update();
 echo $database->get_latest_update();
 
 
-//print_r ($database->conn->info);
-$database->close();
+
+// Get data from iNat
+
+// Test data
+$data = Array();
+$data['sourceId'] = "HR.TEST";
+$data['documents'] = "Json goes here";
+
+
+// POST to API
+$result = postToAPI($data);
+
+if (200 == $result['code']) {
+  echo "Successfully posted to API, which responded 200 and: " . $result['response'];
+  log2("ok", "Posted to API");
+  // Continue processing as usual
+}
+else {
+  echo "Posting to API failed, with responded " . $result['code'] . " and: " . $result['response'];
+  log2("error", "Posting to API failed");
+  // Stop processing
+}
 
 echo " / END";
+$database->close();
 
