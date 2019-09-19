fc26d424d9c080dd87d3a1a7e8970d137f6b8f93
Mikko Heikkinen <hawk@biomi.org>
2019-09-19 15:44:34 +0300

    Add sql with minimum data

--- www/html/_database-schema.sql
+++ www/html/_database-schema.sql
@@ -2,8 +2,8 @@
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
-
-USE `inat_push`;
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
@@ -11,9 +11,11 @@ DROP TABLE IF EXISTS `latest_update`;
 CREATE TABLE `latest_update` (
   `id` int(11) NOT NULL,
   `observation_id` int(11) DEFAULT NULL,
-  `latest_update` varchar(40) COLLATE utf8mb4_swedish_ci DEFAULT NULL
+  `latest_update` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
+INSERT INTO `latest_update` (`id`, `observation_id`, `latest_update`) VALUES
+(0,	0,	'2019-09-30T00:00:00+03:00');
 
 DROP TABLE IF EXISTS `observations`;
 CREATE TABLE `observations` (
@@ -24,4 +26,4 @@ CREATE TABLE `observations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
 
--- 2019-09-26 20:02:36
+-- 2019-09-19 12:43:11
