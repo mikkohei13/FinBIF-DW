baea0ddcb5e707e098911b7be790c1e24c806ca6
Mikko Heikkinen <hawk@biomi.org>
2019-09-27 22:20:06 +0300

    Exit through logger if error

--- www/html/log2.php
+++ www/html/log2.php
@@ -2,7 +2,6 @@
 
 // Note: this is temporarily copied to mysql class.
 function log2($type, $message, $filename = "log/log.txt") {
-
   // https://stackoverflow.com/questions/1252529/get-code-line-and-file-thats-executing-the-current-function-in-php
   $bt = debug_backtrace();
   $caller = array_shift($bt);
@@ -12,5 +11,10 @@ function log2($type, $message, $filename = "log/log.txt") {
   $message = date("Y-m-d H:i:s") . "\t" . $type . "\t" . $caller['file'] . "\t" . $caller['line'] . "\t" . $message . "\n";
 
   $bytes = file_put_contents($filename, $message, FILE_APPEND);
+
+  if ("ERROR" == $type) {
+    exit("Exited through logger");
+  }
+
   return $bytes;
 }
