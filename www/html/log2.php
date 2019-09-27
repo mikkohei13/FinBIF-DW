35702ba6c1474ee3838b3912899547ca3fc0c3a6
Mikko Heikkinen <hawk@biomi.org>
2019-09-27 19:50:57 +0300

    Add file & line number to loggers

--- www/html/log2.php
+++ www/html/log2.php
@@ -2,7 +2,14 @@
 
 // Note: this is temporarily copied to mysql class.
 function log2($type, $message, $filename = "log/log.txt") {
-  $message = date("Y-m-d H:i:s") . "\t" . $type . "\t" . $message . "\n";
+
+  // https://stackoverflow.com/questions/1252529/get-code-line-and-file-thats-executing-the-current-function-in-php
+  $bt = debug_backtrace();
+  $caller = array_shift($bt);
+  // echo $caller['file'];
+  // echo $caller['line'];
+
+  $message = date("Y-m-d H:i:s") . "\t" . $type . "\t" . $caller['file'] . "\t" . $caller['line'] . "\t" . $message . "\n";
 
   $bytes = file_put_contents($filename, $message, FILE_APPEND);
   return $bytes;
